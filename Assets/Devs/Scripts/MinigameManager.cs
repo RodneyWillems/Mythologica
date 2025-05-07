1cdd85fad80ee4822fd01dceda816ad492219ec4
Vincent van Bruggen <lachenbekkie2701@gmail.com>
2025-05-07 09:34:00 +0200

    Started MinigameManager
    working on manager for orpheus minigame

---
+++ Assets/Devs/Scripts/MinigameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    private void SpawnRock()
+    {
+        
+    }
+}
