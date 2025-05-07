497065c2a97beb964e266f42f7d0a1365384a1e9
GohanBlade <rodneywillems@gmail.com>
2025-05-07 12:59:27 +0200

    Updated Orpheus

--- Assets/Devs/Scripts/Minigames/Orpheus/Rock.cs
+++ Assets/Devs/Scripts/Minigames/Orpheus/Rock.cs
@@ -2,15 +2,70 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Vector3 _correctDirection;
+
+    private Vector3 _firstDirection;
+    private Vector3 _targetPosition;
+    private bool _move;
+    private bool _mashing;
+
+    private void Start()
     {
-        
+        _targetPosition = transform.position + (_correctDirection * 6);
+        _firstDirection = _correctDirection;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckMove(Vector3 direction, bool doneMashing = false)
     {
-        
+        if (direction == _correctDirection && !_mashing)
+        {
+            _move = true;
+            return true;
+        }
+        else if (!_mashing)
+        {
+            _mashing = true;
+            switch (Random.Range(0, 4))
+            {
+                case 0:
+                    _correctDirection = Vector2.up;
+                    break;
+                case 1:
+                    _correctDirection = Vector2.down;
+                    break;
+                case 2:
+                    _correctDirection = Vector2.right;
+                    break;
+                case 3:
+                    _correctDirection = Vector2.left;
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+        else if (_mashing && direction == _correctDirection && !doneMashing)
+        {
+            return true;
+        }
+        else if (doneMashing) 
+        {
+            _move = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        if (_move)
+        {
+            transform.position += _correctDirection / 20;
+            if (Vector3.Distance(transform.position, _targetPosition) <= 1)
+            {
+                _move = false;
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Devs/Scripts/Minigames/Players/Orpheus.cs
+++ Assets/Devs/Scripts/Minigames/Players/Orpheus.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Orpheus : MonoBehaviour
 {
-    [SerializeField] private LayerMask m_rockLayer;
+    [SerializeField] private LayerMask _rockLayer;
 
+    [Header("Failed")]
+    [SerializeField] private int _failedInputsNeeded;
+
+    private Rock _failedRock;
+    private int _currentFailedInputs;
+    private bool _failed;
+    private Coroutine _failRoutine;
+
+    // Moving
+    private Coroutine _movingRoutine;
     private Minigames _controls;
+
+    // Misc
+    private MinigameManager _minigameManager;
 
     #region Setup
 
@@ -16,9 +30,15 @@ public class Orpheus : MonoBehaviour
         _controls.Orpheus.Moverock.performed += MoveRock;
     }
 
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
     void Start()
     {
-
+        StartMinigame();
+        _minigameManager = FindFirstObjectByType<MinigameManager>();
     }
 
     #endregion
@@ -28,10 +48,63 @@ public class Orpheus : MonoBehaviour
     private void MoveRock(InputAction.CallbackContext context)
     {
         Vector2 input = _controls.Orpheus.Moverock.ReadValue<Vector2>();
-        if (Physics.Raycast(transform.position, transform.forward, 0.5f, m_rockLayer))
+        if (Physics.BoxCast(transform.position, (Vector3.up + Vector3.right) * 4, transform.forward, out RaycastHit hit, Quaternion.identity, 1))
         {
-
+            if (hit.collider.GetComponent<Rock>().CheckMove(input) && !_failed)
+            {
+                print("Correct input for rock!");
+                if (_movingRoutine == null)
+                {
+                    _movingRoutine = StartCoroutine(MoveForward());
+                }
+            }
+            else if (hit.collider.GetComponent<Rock>().CheckMove(input) && _failed)
+            {
+                print("Right input for mashing!");
+                _currentFailedInputs++;
+            }
+            else
+            {
+                print("Wrong input start mashing!!!");
+                if (_failRoutine == null)
+                {
+                    _failedRock = hit.collider.GetComponent<Rock>();
+                    _failed = true;
+                    _failRoutine = StartCoroutine(Failed());
+                }
+            }
         }
+    }
+
+    private IEnumerator MoveForward()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Vector3 targetPosition = transform.position + transform.forward;
+        while (Vector3.Distance(transform.position, targetPosition) >= 0.1f)
+        {
+            transform.position += transform.forward / 8;
+            yield return new WaitForEndOfFrame();
+        }
+        _movingRoutine = null;
+    }
+
+    private IEnumerator Failed()
+    {
+        while (_currentFailedInputs < _failedInputsNeeded)
+        {
+            _failed = true;
+            yield return new WaitForEndOfFrame();
+        }
+        print("Finished mashing");
+        if (_movingRoutine == null)
+        {
+            _movingRoutine = StartCoroutine(MoveForward());
+        }
+        _currentFailedInputs = 0;
+        _failedRock.CheckMove(Vector2.up, true);
+        _failed = false;
+        _failRoutine = null;
+        yield return null;
     }
 
     #endregion
@@ -39,6 +112,11 @@ public class Orpheus : MonoBehaviour
     public void StartMinigame()
     {
         _controls.Orpheus.Enable();
+    }
+
+    public void EndMinigame()
+    {
+        _controls.Orpheus.Disable();
     }
 
     void Update()
